64b3ae71d7d420951586c047b4bbab15679053d1
leobez <leossbezerra@gmail.com>
2024-05-01 14:39:45 -0300

    progress on content

--- client-side-react/src/App.jsx
+++ client-side-react/src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Page1 from './pages/Page1'
-import Page2 from './pages/Page2'
-import Page3 from './pages/Page3'
-import Page4 from './pages/Page4'
+import Page404 from './pages/Page404'
+import SEO from './pages/SEO'
+import Renderizacao from './pages/Renderizacao'
+import Media from './pages/Media'
+import Web from './pages/Web'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="h-full flex flex-col">
+    <div>
 
       <BrowserRouter>
 
-        <header className="flex-none h-20 bg-slate-950 grid place-items-center">
+        <header className="h-20 bg-slate-950 grid place-items-center">
 
           <div className="w-10/12 flex justify-between">
 
@@ -27,10 +26,10 @@ function App() {
             <nav className="border-2 border-slate-200 grid p-1">
               <ul className="flex text-slate-200 p-1">
                 <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/page1">Page1</Link></li>
-                <li className="nav-item"><Link to="/page2">Page2</Link></li>
-                <li className="nav-item"><Link to="/page3">Page3</Link></li>
-                <li className="nav-item"><Link to="/page4">Page4</Link></li>
+                <li className="nav-item"><Link to="/web">Web</Link></li>
+                <li className="nav-item"><Link to="/seo">SEO</Link></li>
+                <li className="nav-item"><Link to="/render">Renderização</Link></li>
+                <li className="nav-item"><Link to="/media">Mídia</Link></li>
               </ul>
             </nav>
 
@@ -38,21 +37,22 @@ function App() {
 
         </header>
 
-        <div className="max-h-full flex-grow w-full flex justify-center bg-slate-200">
-          <div className="max-w-6xl min-w-[1152px] bg-white p-4 shadow-lg">
+        <div className="max-h-full w-full flex justify-center bg-slate-200">
+          <div className="max-w-4xl xl:min-w-[896px] bg-white p-4 shadow-lg">
             <Routes>
               <Route path='/' element={<Home/>}></Route>
-              <Route path='/page1' element={<Page1/>}></Route>
-              <Route path='/page2' element={<Page2/>}></Route>
-              <Route path='/page3' element={<Page3/>}></Route>
-              <Route path='/page4' element={<Page4/>}></Route>
+              <Route path='/web' element={<Web/>}></Route>
+              <Route path='/seo' element={<SEO/>}></Route>
+              <Route path='/render' element={<Renderizacao/>}></Route>
+              <Route path='/media' element={<Media/>}></Route>
+              <Route path='*' element={<Page404/>}></Route>
             </Routes>
           </div>
         </div>
       
       </BrowserRouter>
 
-      <footer className="flex-none h-20 bg-slate-950 grid place-items-center">
+      <footer className="h-20 bg-slate-950 grid place-items-center">
 
         <div className="w-10/12 flex justify-between">
           <div className="flex flex-col gap-2">
