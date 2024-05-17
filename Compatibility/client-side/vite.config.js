97227b1e6df6a1b1203b08af66380249bb78a9af
leobez <leossbezerra@gmail.com>
2024-05-17 19:43:30 -0300

    update on compatibility folder

--- Compatibility/client-side/vite.config.js
+++ Compatibility/client-side/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'sitemap.xml', // Caminho relativo ao diretório raiz do projeto
+          dest: '' // Destino relativo à pasta de saída (dist)
+        }
+      ]
+    })
+  ],
 })
