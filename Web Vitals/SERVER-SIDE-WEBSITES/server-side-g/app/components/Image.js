d2caf5d65ce6048cebf1166e141b491885dfc0e6
leobez <leossbezerra@gmail.com>
2024-05-21 14:17:22 -0300

    progress on web vitals testing

---
+++ Web Vitals/SERVER-SIDE-WEBSITES/server-side-g/app/components/Image.js
@@ -0,0 +1,58 @@
+const Image = () => {
+  return (
+    <div className='flex flex-col gap-4'>
+        <p className='font-bold'>Imagens</p>
+        <div className="flex flex-wrap w-full ">
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img1.png' alt="img1" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img2.png' alt="img2" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img3.png' alt="img3" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img4.png' alt="img4" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img5.png' alt="img5" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img6.png' alt="img6" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img7.png' alt="img7" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img8.png' alt="img8" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img9.png' alt="img9" className='w-full h-full object-cover' loading='lazy'/></div>
+            
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img10.png' alt="img10" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img11.png' alt="img11" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img12.png' alt="img12" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img13.png' alt="img13" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img14.png' alt="img14" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img15.png' alt="img15" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img16.png' alt="img16" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img17.png' alt="img17" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img18.png' alt="img18" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img19.png' alt="img19" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img20.png' alt="img20" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img21.png' alt="img21" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img22.png' alt="img22" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img23.png' alt="img23" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img24.png' alt="img24" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img25.png' alt="img25" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img26.png' alt="img26" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img27.png' alt="img27" className='w-full h-full object-cover' loading='lazy'/></div>
+            
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img28.png' alt="img28" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img29.png' alt="img29" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img30.png' alt="img30" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img31.png' alt="img31" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-4/6 bg-white shadow border-2 border-white"><img src='/images/img32.png' alt="img32" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img33.png' alt="img33" className='w-full h-full object-cover' loading='lazy'/></div>
+
+            <div className="max-h-[400px] w-3/6 bg-white shadow border-2 border-white"><img src='/images/img34.png' alt="img34" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-1/6 bg-white shadow border-2 border-white"><img src='/images/img35.png' alt="img35" className='w-full h-full object-cover' loading='lazy'/></div>
+            <div className="max-h-[400px] w-2/6 bg-white shadow border-2 border-white"><img src='/images/img36.png' alt="img36" className='w-full h-full object-cover' loading='lazy'/></div>
+        </div>
+    </div>
+  )
+}
+
+export default Image
